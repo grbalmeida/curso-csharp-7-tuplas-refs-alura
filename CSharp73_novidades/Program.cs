defacefd91adbe373aeb5a3f6fd43e5cf3c7cb9c
grbalmeida <g.r.almeida@live.com>
2022-10-09 20:08:08 -0300

    Igualdade entre tuples

---
+++ CSharp73_novidades/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp73_novidades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TesteTuples.TestaTuple();
+        }
+    }
+}
---
+++ CSharp73_novidades/TesteTuples.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CSharp73_novidades
+{
+    static class TesteTuples
+    {
+        public static void TestaTuple()
+        {
+            var origem = ("Avenida Paulista", "900", "São Paulo");
+            var destino = ("Avenida Paulista", "300", "São Paulo");
+
+            // Comparação Tupla C# 7.2
+            var mesmoLugar =
+                origem.Item1 == destino.Item1 &&
+                origem.Item2 == destino.Item2 &&
+                origem.Item3 == destino.Item3;
+
+            Console.WriteLine(mesmoLugar);
+
+            // Comparação Tupla C# 7.3
+            mesmoLugar = origem == destino;
+            Console.WriteLine(mesmoLugar);
+        }
+    }
+}
